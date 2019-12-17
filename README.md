56ade720f031834f25ded4e9b0ea35ed340fe8f0
toshiya kawasaki <kawasakitoshiya@gmail.com>
2019-12-17 19:19:20 +0900

    echo server

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-xdf-cli
+xxx
 ---
 Cli toolfor reading and converting multiple data formats like Apache parquest, Aparche avro, Aparche ORC, pandas pickle and more; written in rust.
+
+
+### Development
+Use nightly channel of rust
+```
+$ rustup update
+$ rustup override set nightly
+$ rustup update nightly
+```
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
-extern crate clap;
-use clap::{App, Arg};
+use std::io::{Read, Write};
+use std::net::TcpListener;
+use std::thread;
 
 fn main() {
-    let matches = App::new("xdf-cli")
-        .version("1.0")
-        .about("CLI tools for reading and converting multiple data formats")
-        .arg(
-            Arg::with_name("INPUT_FILEPATH")
-                .help("Sets the input file to use")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("OUTPUT_FILEPATH")
-                .help("Sets the output to converted")
-                .short("o")
-                .index(2),
-        )
-        .get_matches();
+    let ip = "127.0.0.1";
+    let port = 9123;
+    let bind_addr = format!("{}:{}", ip, port);
+    let listener: Result<TcpListener, _> = TcpListener::bind(bind_addr);
 
-    let input_filepath = matches.value_of("INPUT_FILEPATH").unwrap();
-    println!("Using input file: {}", input_filepath);
-    if let Some(output_filepath) = matches.value_of("OUTPUT_FILEPATH") {
-        println!("Using output file: {}", output_filepath);
+    // Try to bind local address
+    let listener = match listener {
+        Ok(l) => l,
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    };
+    println!("listening started, ready to accept");
+
+    for stream in listener.incoming() {
+        thread::spawn(|| {
+            let mut buf = [0; 1024];
+            let mut stream = stream.unwrap();
+            println!("reading...");
+            println!("Received {} bytes", stream.read(&mut buf).unwrap());
+            let res = buf.iter().map(|&s| s as char).collect::<String>();
+            //            let converted: String = String::from_utf8(buf.to_vec()).unwrap();
+            let tcp_res = format!("{}\r\n", res);
+            stream.write(tcp_res.as_bytes()).unwrap();
+        });
     }
 }
